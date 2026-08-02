a36ca7383bc1d4f76a9606de7fd74d0d5570c8db
Ogundare Sam <ogundaresamson03@gmail.com>
2026-08-02 19:02:18 +0100

    Completed the checked property Tutorial

---
+++ checkedproperty.js
@@ -0,0 +1,34 @@
+// .checked = property that detrmins the actual state of an 
+//           HTML checkbox or radio button element
+
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const verveBtn = document.getElementById("verveBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function() {
+
+    if (myCheckBox.checked){
+        subResult.textContent = `You are Subscribed`;
+    }
+    else {
+        subResult.textContent = `You are not Subscribed`;
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are Paying with Visa`;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are Paying with MasterCard`
+    }
+     else if(verveBtn.checked){
+        paymentResult.textContent = `You are Paying with Verve`
+    }
+    else{
+        paymentResult.textContent = `Select Payment Method`
+    }
+
+}
